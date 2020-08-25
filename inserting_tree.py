77dc2b759e9988bccd8f35b20c93dddcb68e74f1
vantage-ola <vantageola4@gmail.com>
2020-08-25 09:31:45 +0100

    added runtime

--- inserting_tree.py
+++ inserting_tree.py
@@ -1,12 +1,23 @@
 from binarytree import Tree
 from treenode import Node
-tree=Tree()
-tree.insert(1)
-tree.insert(5)
-tree.insert(2)
-tree.insert(9)
-tree.insert(7)
+import time
 
-for i in range(1, 10):
-    found= tree.search(i)
-    print("[] : []".format(i, found))
+start = time.time()
+def inserttree():
+    tree=Tree()
+    tree.insert(1)
+    tree.insert(5)
+    tree.insert(2)
+    tree.insert(9)
+    tree.insert(7)
+
+    for i in range(1, 10):
+        found= tree.search(i)
+        print("[] : []".format(i, found))
+end = time.time()    
+time=end-start
+print("time elasped:", time)
+
+
+import cProfile
+cProfile.run('inserttree()')
